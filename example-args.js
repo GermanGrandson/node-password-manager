73bb2f36063e6f3ff8448773b737f75330e25f32
German Grandson <ganieto1@gmail.com>
2017-04-23 17:21:03 -0400

    add yarg package

---
+++ example-args.js
@@ -0,0 +1,36 @@
+var argv = require('yargs')
+    .command('platanos', 'Greets the user', function(yargs){
+      yargs.options({
+        name: {
+            demand: true,
+            alias: 'g',
+            description: 'Gets first name'
+          },
+        lastName: {
+          demand: true,
+          alias: 'l',
+          description: 'Gets last name'
+        }
+      }).help('help')
+    })
+    .help('help')
+    .argv;
+var command = argv._[0];
+// var lastName = argv.lastName;
+console.log(argv);
+
+// if(argv._[0] == 'platanos'){
+//   console.log('Platanos RUle!');
+// }
+
+// if(command == 'platanos' && typeof argv.name !== 'undefined'){
+//   console.log('YOOO ' + argv.name);
+// } else if(command == 'platanos'){
+//   console.log('YOOO MTV RAPS!')
+// }
+
+if(command == 'platanos' && typeof lastName !== 'undefined'){
+  console.log('Want some platanos ' + argv.name + ' ' + lastName );
+} else if (command == 'platanos'){
+  console.log('Want some platanos ' + argv.name);
+}
